c4e49010fe29de9772611b21b27ff7ad2252d58e
medaitswa <medaitswa@gmail.com>
2023-10-27 00:05:59 +0200

    42

--- server.cpp
+++ server.cpp
@@ -51,11 +51,12 @@ std::string	&webserv::getBuffer( void ) {
 void	webserv::FillHeaders_( st_ request_ ) {
 	for (int i = 0; request_[i] && !request_.empty(); i++) {
 		size_t found_it = request_.find(": ");
-		size_t found_end = request_.find("\r\n");
-		if ((found_it != std::string::npos && found_end != std::string::npos)) {
+		if (found_it != std::string::npos) {
 			st_ key = request_.substr(0, found_it);
-			std::cout << key << std::endl;
 			request_.erase(0, found_it + 2);
+			size_t found_end = request_.find("\r\n");
+			if (found_end == std::string::npos)
+				break ;
 			st_ value = request_.substr(0, found_end);
 			request_.erase(0, found_end + 2);
 			headers.push_back(std::make_pair(key, value));
@@ -85,22 +86,22 @@ void webserv::HTTPRequest( void ) {
 	FillHeaders_(request);
 }
 void	webserv::printVec(void) {
-	// std::cout << "Method : " << getMethod_() << " URI : " << getURI() << " V : " << getVersion() << std::endl;
-	// for (std::vector < std::pair < st_, st_ > >::iterator it_ = headers.begin(); it_ != headers.end(); it_++)
-	// 	std::cout << it_->first << " ->> " << it_->second << std::endl;
+	std::cout << "Method : " << getMethod_() << " URI : " << getURI() << " V : " << getVersion() << std::endl;
+	for (std::vector < std::pair < st_, st_ > >::iterator it_ = headers.begin(); it_ != headers.end(); it_++)
+		std::cout << it_->first << " ->> " << it_->second << std::endl;
 }
 void	webserv::set_up( void ) { 
 	char	buffer[1024];
 	int	_socket_cl, _socket_ser;
-	const char *port = "2050";
+	// const char *port = "2050";
 	struct sockaddr_in server_, client_;
 	_socket_ser = socket(AF_INET, SOCK_STREAM, 0);
 	server_.sin_family = AF_INET;
-	server_.sin_port = htons(atoi(port));
+	server_.sin_port = htons((2050));
 	server_.sin_addr.s_addr = INADDR_ANY;
 	bind(_socket_ser, (struct sockaddr *)&server_, sizeof(server_));
 	listen(_socket_ser, 5);
-	std::cout << BOLD_GREEN << "Server is listenning at Port " << port << " ->" << DEF << std::endl;
+	std::cout << BOLD_GREEN << "Server is listenning at Port 2050 " << " ->" << DEF << std::endl;
 	socklen_t length = sizeof(client_);
 	_socket_cl = accept(_socket_ser, (struct sockaddr *)&client_, &length);
 	recv(_socket_cl, buffer, 1024, 0);
