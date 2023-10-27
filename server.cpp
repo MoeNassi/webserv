1c65e83243a5653ae016714d80cd8ecc5aa0afa8
Mohammed Nassi <mnassi@e2r3p8.1337.ma>
2023-10-27 19:10:54 +0100

    42

--- server.cpp
+++ server.cpp
@@ -52,11 +52,13 @@ void	webserv::CheckForBody( st_ request_ ) {
 	std::vector < std::pair < st_, st_ > >::iterator it_ = headers.begin();
 	for (; it_ != headers.end(); it_++) {
 		if ((!it_->first.compare("Content-Length") && atoi(it_->second.c_str()) > 0)
-			|| (!it_->first.compare("Transfer-Encoding") && !it_->second.compare("chunked")))
-			setBody(request_);
+			|| (!it_->first.compare("Transfer-Encoding") && !it_->second.compare("chunked"))) { //if it exist and method is post and its value is diff to "chunked" : 501 Not Implimented
+				request_.erase(0, request_.find("\r\n") + 2);
+				setBody(request_);
+		}
+	if (it_ == headers.end() && !getMethod_().compare("POST"))
+		perror("400 Bad Request\n");
 	}
-	if (it_ == headers.end() && !getMethod_().compare("POST") )
-		perror("501 Not Implimented\n");
 }
 void	webserv::FillHeaders_( st_ request_ ) {
 	for (int i = 0; request_.substr(0, 2) != "\r\n" && !request_.empty(); i++) {
@@ -65,7 +67,7 @@ void	webserv::FillHeaders_( st_ request_ ) {
 			st_ key = request_.substr(0, found_it);
 			request_.erase(0, found_it + 2);
 			size_t found_end = request_.find("\r\n");
-			if (found_end == std::string::npos)
+			if (found_end == std::string::npos || key.empty())
 				break ;
 			st_ value = request_.substr(0, found_end);
 			request_.erase(0, found_end + 2);
@@ -96,14 +98,14 @@ void webserv::HTTPRequest( void ) {
 	FillHeaders_(request);
 }
 void	webserv::printVec(void) {
-	std::cout << "Method : " << getMethod_() << " URI : " << getURI() << " V : " << getVersion() << "Body : " << getBody() << std::endl;
+	std::cout << "Method : " << getMethod_() << " URI : " << getURI() << " V : " << getVersion() << " Body : " << getBody() << std::endl;
 	for (std::vector < std::pair < st_, st_ > >::iterator it_ = headers.begin(); it_ != headers.end(); it_++)
 		std::cout << it_->first << " ->> " << it_->second << std::endl;
 }
 void	webserv::set_up( void ) { 
 	char	buffer[1024];
 	int	_socket_cl, _socket_ser;
-	const char *port = "2050";
+	const char *port = "8080";
 	struct sockaddr_in server_, client_;
 	_socket_ser = socket(AF_INET, SOCK_STREAM, 0);
 	server_.sin_family = AF_INET;
@@ -111,11 +113,11 @@ void	webserv::set_up( void ) {
 	server_.sin_addr.s_addr = INADDR_ANY;
 	bind(_socket_ser, (struct sockaddr *)&server_, sizeof(server_));
 	listen(_socket_ser, 5);
-	std::cout << BOLD_GREEN << "Server is listenning at Port 2050 " << " ->" << DEF << std::endl;
+	std::cout << BOLD_GREEN << "Server is listenning at Port " << port << " ->" << DEF << std::endl;
 	socklen_t length = sizeof(client_);
 	_socket_cl = accept(_socket_ser, (struct sockaddr *)&client_, &length);
 	recv(_socket_cl, buffer, 1024, 0);
-	std::cout << BOLD_RED << "Request : \n" << DEF << buffer << std::endl;
+	// std::cout << BOLD_RED << "Request : \n" << DEF << buffer << std::endl;
 	const char *response = "Received";
 	send(_socket_cl, response, static_cast<std::string>(response).length(), 0);
 	setBuffer(static_cast<std::string>(buffer));
